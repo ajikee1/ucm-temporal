2317edc2009eaf9c000ec2996cfdfb456c2b0864
ajikee1 <akeeri1@students.towson.edu>
2022-01-08 23:01:32 -0500

    json in springboot post to trigger workflow

--- src/main/java/rest/Routes.java
+++ src/main/java/rest/Routes.java
@@ -4,31 +4,52 @@ import io.temporal.api.common.v1.WorkflowExecution;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.serviceclient.WorkflowServiceStubs;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import netscape.javascript.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
 import temporal.UcmWorkFlow;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Routes {
 
-    @RequestMapping(value="/initiateWorkFlow/", method = RequestMethod.POST)
-    public void initiate(){
-        String issueId = "DIS-1";
+    @RequestMapping(value = "/initiateWorkFlow/", method = RequestMethod.POST)
+    public void initiateWorkFlow(@RequestBody String request) {
 
         WorkflowServiceStubs service = WorkflowServiceStubs.newInstance();
         WorkflowClient client = WorkflowClient.newInstance(service);
 
-        WorkflowOptions options = WorkflowOptions.newBuilder().setWorkflowId(issueId).setTaskQueue("JENKINS_TASK_QUEUE").build();
-        UcmWorkFlow workflow = client.newWorkflowStub(UcmWorkFlow.class, options);
-
-
         List<String> jobList = new ArrayList<>();
-        jobList.add("nodeTest");
-        jobList.add("temporal_demo");
 
-        WorkflowExecution we = WorkflowClient.start(workflow::initiateWorkFlow, jobList, issueId);
+        try {
+            JSONObject requestObj = new JSONObject(request);
+
+            String issueId = requestObj.get("jiraTicketId").toString();
+
+            WorkflowOptions options = WorkflowOptions.newBuilder().setWorkflowId(issueId).setTaskQueue("JENKINS_TASK_QUEUE").build();
+            UcmWorkFlow workflow = client.newWorkflowStub(UcmWorkFlow.class, options);
+
+            JSONArray jobsArray = requestObj.getJSONArray("jobList");
+
+            if (jobsArray.length() > 0) {
+                for (int i = 0; i < jobsArray.length(); i++) {
+                    JSONObject jobsObject = jobsArray.getJSONObject(i);
+                    String jobName = jobsObject.getString("jobName");
+
+                    jobList.add(jobName);
+                }
+            }
+
+            if (!jobList.isEmpty()) {
+                WorkflowExecution we = WorkflowClient.start(workflow::initiateWorkFlow, jobList, issueId);
+            }
+
+        } catch (Exception e) {
+        }
+
+
     }
 }
